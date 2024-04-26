65ae81f20066e26e1a17847ba49e73e9213db8a8
Sapar Sayat <saparsayat01@gmail.com>
2024-04-26 21:57:28 +0500

    feat: create articleservice.kt

--- src/main/kotlin/com/nomadbala/jwtTokens/service/ArticleService.kt
+++ src/main/kotlin/com/nomadbala/jwtTokens/service/ArticleService.kt
@@ -4,10 +4,14 @@ import com.nomadbala.jwtTokens.model.Article
 import com.nomadbala.jwtTokens.repository.ArticleRepository
 import org.springframework.stereotype.Service
 
+interface IArticleService {
+    fun findAll(): List<Article>
+}
+
 @Service
 class ArticleService(
     private val repository: ArticleRepository
-) {
-    fun findAll(): List<Article> =
+) : IArticleService {
+    override fun findAll(): List<Article> =
         repository.findAll()
 }
