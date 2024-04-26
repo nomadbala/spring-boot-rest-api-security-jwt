c38b7e1128a393619f13e8cc823b6dbce0f3a295
Sapar Sayat <saparsayat01@gmail.com>
2024-04-26 16:08:10 +0500

    init: create spring-boot-security-jwt project

---
+++ src/test/kotlin/com/nomadbala/jwtTokens/JwtTokensApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nomadbala.jwtTokens
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtTokensApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
